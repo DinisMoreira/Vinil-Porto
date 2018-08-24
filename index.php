e0a5177fc519ff11db28ca029f8a0a2c021f6db9
Unknown <dinismoreira1997@gmail.com>
2018-08-24 17:33:39 +0100

    Init

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE>
+<html>
+  <head>
+    <title> Vinil Porto </title>
+  </head>
+  <body>
+    <header>
+      <h1>Vinil Porto</h1>
+      <select name = "Ordenação">
+        <option value = "Preço mais baixo">Preço mais baixo</option>
+        <option value = "Preço mais alto">Preço mais alto</option>
+        <option value = "Alfabética">Alfabética</option>
+      </select>
+    </header>
+  </body>
+</html>
