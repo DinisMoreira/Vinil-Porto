f0e5d32ec55fe143655a02b3b3ff134d930ee7ae
up201606214@FEUPSIG.FE.UP.PT <up201606214@FEUPSIG.FE.UP.PT>
2018-08-27 18:33:52 +0100

    Added Login.php and VerifyLogin.php

---
+++ Code/VerifyLogin.php
@@ -0,0 +1,26 @@
+﻿<!DOCTYPE>
+<html>
+  <head>
+    <title> Vinil Porto - Login </title>
+  </head>
+  <body>
+    <header>
+      <h1>Vinil Porto</h1>
+	    <form action="" method="get">
+        <label> Username:
+          <input type="text" name="username">
+        </label>
+        <br>
+        <br>
+        <label> Password:
+          <input type="password" name="password">
+        </label>
+        <br>
+        <br>
+        
+        <input type="submit" value ="Ok">
+        
+	    </form>
+    </header>
+  </body>
+</html>
